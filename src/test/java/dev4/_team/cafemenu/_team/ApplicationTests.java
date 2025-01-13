546a353fc37a053560a1a6543e6ca2fcf7cc1a16
JEONGJAEIK <wodlr1207@naver.com>
2025-01-13 17:26:21 +0900

    feat : 초기세팅

---
+++ src/test/java/dev4/_team/cafemenu/_team/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev4._team.cafemenu._team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
