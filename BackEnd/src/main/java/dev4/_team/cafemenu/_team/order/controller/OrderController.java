c5497435a6350506bd41e538297fb19082958e9a
JEONGJAEIK <wodlr1207@naver.com>
2025-01-16 17:21:37 +0900

    feat : 주문 조회 기능 수정 중 feat : BaseTimeEntity 시간 연동

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/controller/OrderController.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/controller/OrderController.java
@@ -2,11 +2,13 @@ package dev4._team.cafemenu._team.order.controller;
 
 import dev4._team.cafemenu._team.order.entity.Order;
 import dev4._team.cafemenu._team.order.service.OrderService;
+import dev4._team.cafemenu._team.orderProduct.entity.OrderProduct;
+import dev4._team.cafemenu._team.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/order")
@@ -18,10 +20,16 @@ public class OrderController {
     @PostMapping
     public Order createOrder(@RequestBody Order order) {
         return orderService.createOrder(order);
+    }
 
     @DeleteMapping
     public void deleteOrder(@RequestBody Order order) {
         orderService.delete(order);
     }
+
+//    @GetMapping
+//    public List<Order> getOrders(Long id) {
+//        return orderService.getOrder(id);
+//    }
 
 }
--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/service/OrderService.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/service/OrderService.java
@@ -5,10 +5,15 @@ import dev4._team.cafemenu._team.error.BusinessException;
 import dev4._team.cafemenu._team.error.ErrorCode;
 import dev4._team.cafemenu._team.order.entity.Order;
 import dev4._team.cafemenu._team.order.repository.OrderRepository;
+import dev4._team.cafemenu._team.orderProduct.entity.OrderProduct;
+import dev4._team.cafemenu._team.user.entity.User;
+import dev4._team.cafemenu._team.user.repository.UserRepository;
+import dev4._team.cafemenu._team.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -16,6 +21,8 @@ import java.util.Optional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     public Order createOrder(Order order) {
         Order newOrder = Order.builder()
@@ -32,21 +39,21 @@ public class OrderService {
         return newOrder;
     }
 
-    public Order getOrder(Long id) {
-        Optional<Order> order = orderRepository.findById(id);
-        if (order.isEmpty()) {
+    public User getOrder(Long id) {
+        Optional<User> userOrder = userRepository.findOrderListById(id);
+        if (userOrder.isEmpty()) {
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR, "order not found");
         } else {
-            return order.get();
+            return userOrder.get();
         }
     }
 
-    public void delete(Order order) {
-        Optional<Order> deleteorder = (orderRepository.findOrderById(order.getId()));
-        if (deleteorder.isEmpty()) {
-            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR, "order not found");
-        } else {
-            orderRepository.delete(deleteorder.get());
-        }
-    }
+//    public void delete(Order order) {
+//        Optional<Order> deleteorder = (orderRepository.finduserBy(order.getId()));
+//        if (deleteorder.isEmpty()) {
+//            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR, "order not found");
+//        } else {
+//            orderRepository.delete(deleteorder.get());
+//        }
+//    }
 }
