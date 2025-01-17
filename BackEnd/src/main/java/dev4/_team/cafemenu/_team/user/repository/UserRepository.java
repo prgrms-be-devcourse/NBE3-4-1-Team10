d3859b6a25e0f9bc31ab6c38ece5d45d7aa0ea7b
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-01-17 16:40:11 +0900

    fix: 잘못 머지된 코드 수정

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/repository/UserRepository.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/repository/UserRepository.java
@@ -5,11 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-<<<<<<< HEAD
-public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findOrderListById(Long id);
-=======
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
->>>>>>> user
 }
