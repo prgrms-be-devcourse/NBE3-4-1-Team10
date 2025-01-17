57511af017be981b3e58bcadab346df5152166f3
jho951 <jho951@naver.com>
2025-01-17 11:11:26 +0900

    feat:회원가입 기능

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/service/UserService.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/service/UserService.java
@@ -1,7 +1,10 @@
 package dev4._team.cafemenu._team.user.service;
 
+import dev4._team.cafemenu._team.user.dto.SignupDto;
+import dev4._team.cafemenu._team.user.entity.User;
 import dev4._team.cafemenu._team.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +12,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    // 회원가입 처리
+    public void signup(SignupDto signupDto) {
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(signupDto.getPassword());
+
+        // User 엔티티 생성
+        User user = new User(signupDto.getEmail(), signupDto.getNickname(), encodedPassword);
+
+        // 이메일 중복 체크
+        if (userRepository.existsByEmail(signupDto.getEmail())) {  // 인스턴스를 통해 메소드 호출
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+        // 사용자 저장
+        userRepository.save(user);
+    }
 }
