ffc320f966908088e3506ba92e5031da479d72f2
강정수 <emdtod12@naver.com>
2025-01-16 19:08:19 -0800

    feat : email로 user 확인하는 메서드 작성

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/repository/UserRepository.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package dev4._team.cafemenu._team.user.repository;
 import dev4._team.cafemenu._team.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
 }
