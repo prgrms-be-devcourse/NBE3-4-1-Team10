2efd6a4cb0977fcb4774ece4b5c0681101f8e0d2
jho951 <jho951@naver.com>
2025-01-17 11:11:13 +0900

    feat:이메일 중복여부

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/repository/UserRepository.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package dev4._team.cafemenu._team.user.repository;
 import dev4._team.cafemenu._team.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,String> {;
+    // 이메일 중복 여부
+    boolean existsByEmail(String email);
 }
+
