6bcce3d5356b1edf18c0e5c36038906b9f46f11d
강정수 <emdtod12@naver.com>
2025-01-16 19:11:10 -0800

    feat : 로그인 관련 errorcode추가

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/global/exception/ErrorCode.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/global/exception/ErrorCode.java
@@ -6,7 +6,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "1", "서버내부 에러가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "1", "서버내부 에러가 발생했습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "11", "유저를 찾을수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"12","비밀번호가 틀렸습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"13","인증이 필요합니다.");
+
+
 
     private final HttpStatus httpStatus;
     private final String errorCode;
