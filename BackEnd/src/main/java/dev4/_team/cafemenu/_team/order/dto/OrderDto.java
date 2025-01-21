ea191c278f8d99e646d112220fede067405107f2
강정수 <emdtod12@naver.com>
2025-01-21 03:10:34 -0800

    feat : totalPrice 추가

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/dto/OrderDto.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/dto/OrderDto.java
@@ -2,6 +2,7 @@ package dev4._team.cafemenu._team.order.dto;
 
 import dev4._team.cafemenu._team.orderProduct.dto.OrderProductDto;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +24,9 @@ public class OrderDto {
     @NotEmpty
     @Size(max = 10, message = "우편번호는 10자를 초과할 수 없습니다.")
     private String post;
+
+    @NotNull
+    private int totalPrice;
 
     private List<OrderProductDto> orderProductDto;
 }
