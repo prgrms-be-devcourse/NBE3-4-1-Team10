f8338502a62f821d00ba67c12ad2132fc959ee54
강정수 <emdtod12@naver.com>
2025-01-21 03:11:43 -0800

    feat : 주문 누적되도록 개발

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/repository/OrderRepository.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package dev4._team.cafemenu._team.order.repository;
 
 import dev4._team.cafemenu._team.order.entity.Orders;
+import dev4._team.cafemenu._team.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,5 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
     Optional<Orders> findOrderById(Long id);
     List<Orders> findByUserId(Long id);
     List<Orders> findByStatus(String status);
+    Optional<Orders> findOrdersByUserAndTimeBetweenAndAddress (User user, LocalDateTime start, LocalDateTime end,String address);
 }
--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/service/OrderService.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/order/service/OrderService.java
@@ -8,7 +8,6 @@ import dev4._team.cafemenu._team.order.entity.Orders;
 import dev4._team.cafemenu._team.order.mapper.OrderMapper;
 import dev4._team.cafemenu._team.order.repository.OrderRepository;
 import dev4._team.cafemenu._team.orderProduct.dto.OrderProductDto;
-import dev4._team.cafemenu._team.orderProduct.dto.OrderProductResponseDto;
 import dev4._team.cafemenu._team.orderProduct.entity.OrderProduct;
 import dev4._team.cafemenu._team.orderProduct.repository.OrderProductRepository;
 import dev4._team.cafemenu._team.product.entity.Product;
@@ -16,16 +15,17 @@ import dev4._team.cafemenu._team.product.repository.ProductRepository;
 import dev4._team.cafemenu._team.user.entity.User;
 import dev4._team.cafemenu._team.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -34,13 +34,56 @@ public class OrderService {
     private final OrderProductRepository orderProductRepository;
 
 
+    @Transactional
     public void createOrder(OrderDto orderDto, Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime start = getTime(now);
+        LocalDateTime end = start.plusDays(1);
 
-        Orders orders = OrderMapper.toEntity(orderDto, user);
-        orderRepository.save(orders);
-        saveOrderProduct(orderDto, orders);
+        if (orderRepository.findOrdersByUserAndTimeBetweenAndAddress(user, start, end, orderDto.getAddress()).isPresent()) {
+            Orders orders = orderRepository.findOrdersByUserAndTimeBetweenAndAddress(user, start, end, orderDto.getAddress()).get();
+            orders.updateTotalPrice(orderDto.getTotalPrice());
+            additionalOrder(orders, orderDto.getOrderProductDto());
+        } else {
+            Orders orders = OrderMapper.toEntity(orderDto, user);
+            orderRepository.save(orders);
+            saveOrderProduct(orderDto, orders);
+        }
+    }
+
+    private void additionalOrder(Orders existingOrder, List<OrderProductDto> orderProductDtos) {
+        List<OrderProduct> existingOrderProducts = existingOrder.getOrderProducts();
+
+        for (OrderProductDto orderProductDto : orderProductDtos) {
+            Long productId = orderProductDto.getProductId();
+
+            Optional<OrderProduct> existingProduct = existingOrderProducts.stream()
+                    .filter(op -> op.getProduct().getId().equals(productId))
+                    .findFirst();
+
+            if (existingProduct.isPresent()) {
+                existingProduct.get().updateCountAndPrice(
+                        existingProduct.get().getCount() + orderProductDto.getCount());
+                log.info("상품수량이 더 추가되었습니다.");
+            } else {
+                Product product = productRepository.findById(productId)
+                        .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_PRODUCT));
+                OrderProduct newOrderProduct = orderProductDto.toOrderProductEntity(existingOrder, product);
+                orderProductRepository.save(newOrderProduct);
+                log.info("새상품이 더 추가되었습니다.");
+            }
+        }
+    }
+
+    private LocalDateTime getTime(LocalDateTime now) {
+        LocalDateTime time = now.withHour(14).withMinute(0).withSecond(0).withNano(0);
+        if (now.isBefore(time)) {
+            return time.minusDays(1);
+        } else {
+            return time;
+        }
     }
 
     private void saveOrderProduct(OrderDto orderDto, Orders orders) {
@@ -89,7 +132,7 @@ public class OrderService {
                 .filter(order -> order.getId().equals(orderId))
                 .findFirst()
                 .orElseThrow(() -> new BusinessException(ErrorCode.ORDER_NOT_FOUND, "해당 주문은 존재하지 않습니다."));
-        findOrder.updateOrder(orderDto.getAddress(), orderDto.getPost());
+        findOrder.updateOrder(orderDto.getAddress(), orderDto.getPost(),orderDto.getTotalPrice());
         // OrderProduct 업데이트
         updateOrderProducts(findOrder, orderDto.getOrderProductDto());
     }
