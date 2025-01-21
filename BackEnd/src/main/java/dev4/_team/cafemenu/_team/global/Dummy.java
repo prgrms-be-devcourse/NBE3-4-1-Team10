7a14f763a27179152dbf4eb73cd1ba0192b030eb
강정수 <emdtod12@naver.com>
2025-01-20 17:42:14 -0800

    feat : user 데이터 추가

--- BackEnd/src/main/java/dev4/_team/cafemenu/_team/global/Dummy.java
+++ BackEnd/src/main/java/dev4/_team/cafemenu/_team/global/Dummy.java
@@ -7,6 +7,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,7 +17,16 @@ public class Dummy {
     private final PasswordEncoder passwordEncoder;
 
     @PostConstruct
+    @Transactional
     public void init() {
+        User user = User.builder()
+                .email("testm@naver.com")
+                .nickname("사용자")
+                .password(passwordEncoder.encode("Rkdwjdtn12!"))
+                .role(UserRole.USER)
+                .build();
+        userRepository.save(user);
+
         User adminUser = User.builder()
                 .email("admin@naver.com")
                 .nickname("관리자")
